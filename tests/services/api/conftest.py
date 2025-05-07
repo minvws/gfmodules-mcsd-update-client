a05538cd6afb15049657c194c3eb4d6563f59d47
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2025-05-07 14:58:03 +0200

    tests: added tests for authentication based api

--- tests/services/api/conftest.py
+++ tests/services/api/conftest.py
@@ -3,7 +3,11 @@ import pytest
 from yarl import URL
 
 from app.config import Config
-from app.services.api.api_service import ApiService
+from app.services.api.api_service import ApiService, AuthenticationBasedApiService
+from app.services.api.authenticators.aws_v4_authenticator import AwsV4Authenticator
+from app.services.api.authenticators.azure_oauth2_authenticator import (
+    AzureOAuth2Authenticator,
+)
 from tests.get_test_config import test_config
 
 config: Final[Config] = test_config()
@@ -27,6 +31,45 @@ def mock_body() -> Dict[str, Any]:
 @pytest.fixture()
 def api_service() -> ApiService:
     return ApiService(
+        timeout=config.supplier_api.timeout,
+        backoff=config.supplier_api.backoff,
+        retries=1,
+    )
+
+
+@pytest.fixture()
+def aws_v4_authenticator() -> AwsV4Authenticator:
+    return AwsV4Authenticator(profile="example", region="example")
+
+
+@pytest.fixture()
+def azure_oauth_authenticator() -> AzureOAuth2Authenticator:
+    return AzureOAuth2Authenticator(
+        token_url="example",
+        client_id="example",
+        client_secret="example",
+        resource="example",
+    )
+
+
+@pytest.fixture()
+def aws_based_api_service(
+    aws_v4_authenticator: AwsV4Authenticator,
+) -> AuthenticationBasedApiService:
+    return AuthenticationBasedApiService(
+        auth=aws_v4_authenticator,
+        timeout=config.supplier_api.timeout,
+        backoff=config.supplier_api.backoff,
+        retries=1,
+    )
+
+
+@pytest.fixture()
+def azure_base_api_service(
+    azure_oauth_authenticator: AzureOAuth2Authenticator,
+) -> AuthenticationBasedApiService:
+    return AuthenticationBasedApiService(
+        auth=azure_oauth_authenticator,
         timeout=config.supplier_api.timeout,
         backoff=config.supplier_api.backoff,
         retries=1,
