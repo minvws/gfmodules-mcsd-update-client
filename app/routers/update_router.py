51dff1fe292a4de1d7c6c71c5e04e763e89a8937
Thomas Bosch <bosch-thomas@protonmail.com>
2025-05-14 10:29:23 +0200

    Added override ignore in update router

--- app/routers/update_router.py
+++ app/routers/update_router.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timezone
-from typing import Any
-from fastapi import APIRouter, Depends
+from typing import Annotated, Any
+from fastapi import APIRouter, Depends, Query
 from pydantic import BaseModel, Field
+from fastapi.exceptions import HTTPException
 
 from app.container import (
     get_supplier_ignored_directory_service,
@@ -23,6 +24,7 @@ class UpdateQueryParams(BaseModel):
 @router.post("", response_model=None, summary="Update all suppliers")
 def update_supplier(
     supplier_id: str | None = None,
+    override_ignore: Annotated[list[str] | None, Query()] = None,
     query_params: UpdateQueryParams = Depends(),
     service: UpdateConsumerService = Depends(get_update_consumer_service),
     supplier_provider: SupplierProvider = Depends(get_supplier_provider),
@@ -31,11 +33,25 @@ def update_supplier(
     since = query_params.since.astimezone(timezone.utc) if query_params.since else None
     if supplier_id is None:
         all_suppliers = supplier_provider.get_all_suppliers()
-        filtered_suppliers = supplier_ignored_directory_service.filter_ignored_supplier_dto(all_suppliers)          
-        data: list[dict[str, Any]] = []
-        for supplier in filtered_suppliers:
-            data.append(service.update(supplier, since))
-        return data
+        ignored_ids = {
+            s.directory_id for s in supplier_ignored_directory_service.get_all_ignored_directories()
+        }
+
+        # Filter out ignored suppliers unless they are explicitly overridden
+        filtered_suppliers = [
+            s for s in all_suppliers if (s.id not in ignored_ids) or (override_ignore is not None and s.id in override_ignore)
+        ]
+
+        return [service.update(supplier, since) for supplier in filtered_suppliers]
 
     supplier = supplier_provider.get_one_supplier(supplier_id)
+    is_ignored = supplier_ignored_directory_service.get_ignored_directory(supplier_id) is not None
+
+
+    if is_ignored and (not override_ignore or supplier_id not in override_ignore):
+            raise HTTPException(
+                status_code=409,
+                detail=f"Supplier {supplier_id} is in the ignore list. Use override_ignore to update.",
+            )
+
     return service.update(supplier, since)
