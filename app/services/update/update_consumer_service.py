349105f1e8a3749094ab3a0b908c0efddb56e600
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2025-05-16 15:25:03 +0200

    refactor: reduce node size and moved computed fields to a separate service

--- app/services/update/update_consumer_service.py
+++ app/services/update/update_consumer_service.py
@@ -5,6 +5,8 @@ from enum import Enum
 import logging
 from typing import List, Any
 from uuid import uuid4
+from app.services.update.computation_service import ComputationService
+from app.services.update.cache.caching_service import InMemoryCachingService
 from fhir.resources.R4B.bundle import Bundle, BundleEntry, BundleEntryRequest
 from yarl import URL
 from app.models.resource_map.dto import ResourceMapDto, ResourceMapUpdateDto
@@ -57,34 +59,40 @@ class UpdateConsumerService:
             timeout, backoff, auth, consumer_url, request_count, strict_validation
         )
         self.__fhir_service = FhirService(strict_validation)
-        self.__cache: List[str] = []
+        self.__cache = InMemoryCachingService()
 
     def cleanup(self, supplier_id: str) -> None:
         for res_type in McsdResources:
-            delete_bundle = Bundle(id=str(uuid4()), type="transaction", entry=[], total=0)
-            resource_map = self.__resource_map_service.find(supplier_id=supplier_id, resource_type=res_type.value)
+            delete_bundle = Bundle(
+                id=str(uuid4()), type="transaction", entry=[], total=0
+            )
+            resource_map = self.__resource_map_service.find(
+                supplier_id=supplier_id, resource_type=res_type.value
+            )
             for res_map_item in resource_map:
                 delete_bundle.entry.append(
                     BundleEntry(
                         request=BundleEntryRequest(
                             method="DELETE",
-                            url=f"{res_type.value}/{res_map_item.consumer_resource_id}"
+                            url=f"{res_type.value}/{res_map_item.consumer_resource_id}",
                         )
                     )
                 )
                 delete_bundle.total += 1
-            logging.info(f"Removing {delete_bundle.total} items from consumer originating from stale supplier {supplier_id}")
+            logging.info(
+                f"Removing {delete_bundle.total} items from consumer originating from stale supplier {supplier_id}"
+            )
             self.__consumer_fhir_api.post_bundle(delete_bundle)
 
     def update(self, supplier: SupplierDto, since: datetime | None = None) -> Any:
         start_time = time.time()
-        if len(self.__cache) > 0:
-            self.__cache = []
+        if not self.__cache.is_empty():
+            self.__cache.clear()
 
         for res in McsdResources:
             self.update_resource(supplier, res.value, since)
-        results = copy.deepcopy(self.__cache)
-        self.__cache = []
+        results = copy.deepcopy([id for id in self.__cache.data.keys()])
+        self.__cache.clear()
         end_time = time.time()
 
         return {"log": f"updated {len(results)}", "time": end_time - start_time}
@@ -100,12 +108,20 @@ class UpdateConsumerService:
             strict_validation=self.strict_validation,
             url=supplier.endpoint,
         )
+        computation_service = ComputationService(
+            supplier_id=supplier.id,
+            fhir_service=self.__fhir_service,
+            resource_map_service=self.__resource_map_service,
+        )
+
         adjacency_map_service = AdjacencyMapService(
             supplier_id=supplier.id,
             fhir_service=self.__fhir_service,
             supplier_api=supplier_fhir_api,
             consumer_api=self.__consumer_fhir_api,
             resource_map_service=self.__resource_map_service,
+            cache_service=self.__cache,
+            computation_service=computation_service,
         )
         next_url: URL | None = supplier_fhir_api.build_base_history_url(
             resource_type, since
@@ -117,7 +133,7 @@ class UpdateConsumerService:
             for e in history:
                 _, id = self.__fhir_service.get_resource_type_and_id_from_entry(e)
                 if id is not None:
-                    if id in self.__cache:
+                    if self.__cache.key_exists(id):
                         logger.info(
                             f"{id} {resource_type} already processed.. skipping.. "
                         )
@@ -126,14 +142,15 @@ class UpdateConsumerService:
 
             updated_nodes = self.update_page(targets, adjacency_map_service)
             for node in updated_nodes:
-                if node.resource_id not in self.__cache:
-                    self.__cache.append(node.resource_id)
+                if not self.__cache.key_exists(node.resource_id):
+                    node.clear_for_cash()
+                    self.__cache.add_node(node)
 
     def update_page(
         self, entries: List[BundleEntry], adjacency_map_service: AdjacencyMapService
     ) -> List[Node]:
         updated = []
-        adj_map = adjacency_map_service.build_adjacency_map(entries, self.__cache)
+        adj_map = adjacency_map_service.build_adjacency_map(entries)
         for node in adj_map.data.values():
             if node.updated is True:
                 logger.info(
@@ -153,12 +170,12 @@ class UpdateConsumerService:
         dtos = []
 
         for node in nodes:
-            if node.update_status == "equal":
+            if node.status == "equal":
                 logger.info(
                     f"{node.resource_id} {node.resource_type} has not changed, ignoring..."
                 )
 
-            if node.update_status == "ignore":
+            if node.status == "ignore":
                 logger.info(
                     f"{node.resource_id} {node.resource_type} is not needed, ignoring..."
                 )
