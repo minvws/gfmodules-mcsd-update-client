4054a83e3a4bdea012499633ba61c71f61b928ee
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2025-09-01 14:52:46 +0200

    refactor: removed dictionary checks in Fhir Reference module

--- tests/services/fhir/conftest.py
+++ tests/services/fhir/conftest.py
@@ -3,7 +3,7 @@ import pytest
 import copy
 
 from app.services.fhir.fhir_service import FhirService
-from app.services.fhir.model_factory import create_resource
+from app.services.fhir.resources.factory import create_resource
 from app.services.fhir.references.reference_extractor import get_references
 from app.services.fhir.references.reference_namespacer import (
     namespace_resource_reference,
