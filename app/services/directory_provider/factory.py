c3298ec63e0e0d82eff1f50f2d1fd0162d7587fc
Joshua Thijssen <jthijssen@noxlogic.nl>
2025-10-13 10:44:54 +0200

    initial simplifying of the dir providers

--- app/services/directory_provider/factory.py
+++ app/services/directory_provider/factory.py
@@ -1,12 +1,9 @@
-import json
-from typing import List
-
-from app.models.directory.dto import DirectoryDto
 from app.services.api.authenticators.authenticator import Authenticator
 from app.services.api.fhir_api import FhirApi
+from app.services.directory_provider.capability_provider import CapabilityProvider
+from app.services.directory_provider.fhir_provider import FhirDirectoryProvider
 from app.services.entity.directory_info_service import DirectoryInfoService
 from app.services.api.directory_api_service import DirectoryApiService
-from app.services.directory_provider.caching_provider import CachingDirectoryProvider
 from app.services.directory_provider.json_provider import DirectoryJsonProvider
 from app.services.directory_provider.directory_provider import DirectoryProvider
 from app.config import Config
@@ -24,20 +21,22 @@ class DirectoryProviderFactory:
         self.__directory_info_service = directory_info_service
 
     def create(self) -> DirectoryProvider:
+        provider: DirectoryProvider|None = None
+
         # Use JSON file-based provider if directories_file_path is provided
         if (
             self.__directory_config.directories_file_path is not None
             and len(self.__directory_config.directories_file_path) > 1
         ):
-            return DirectoryJsonProvider(
-                directories=self._read_directories_file(
-                    self.__directory_config.directories_file_path
-                ),
+            provider = DirectoryJsonProvider(
+                json_path=self.__directory_config.directories_file_path,
                 directory_info_service=self.__directory_info_service,
-                validate_capability_statement=self.__mcsd_config.check_capability_statement,
             )
+
         elif self.__directory_config.directories_provider_url is not None:
             # Use API-based provider if directories_provider_url is provided
+
+            # Api service to interact with the FHIR server
             api_service = DirectoryApiService(
                 fhir_api=FhirApi(
                     timeout=self.__directory_config.timeout,
@@ -53,12 +52,12 @@ class DirectoryProviderFactory:
                 ),
                 provider_url=self.__directory_config.directories_provider_url,
             )
-            
-            return CachingDirectoryProvider(
-                api_service=api_service,
+
+            provider = FhirDirectoryProvider(
+                api_provider=api_service,
                 directory_info_service=self.__directory_info_service,
-                validate_capability_statement=self.__mcsd_config.check_capability_statement,
             )
+
         else:
             raise ValueError(
                 "Configuration error: Either 'directories_file_path' or 'directories_provider_url' must be provided. "
@@ -66,10 +65,8 @@ class DirectoryProviderFactory:
                 f"directories_provider_url: {self.__directory_config.directories_provider_url}."
             )
 
-    def _read_directories_file(self, directory_urls_path: str) -> List[DirectoryDto]:
-        try:
-            with open(directory_urls_path) as f:
-                data = json.load(f)
-                return [DirectoryDto(**item) for item in data["directories"]]
-        except (FileNotFoundError, json.JSONDecodeError, KeyError) as e:
-            raise ValueError(f"Error processing directory URLs file: {e}")
+        # Wrap with capability filter provider
+        return CapabilityProvider(
+            inner=provider,
+            validate_capability_statement=self.__mcsd_config.check_capability_statement,
+        )
