fdd604f8ac359e4d77865083300fdb66846667ac
yob1997 <yob1997@live.nl>
2026-01-16 12:49:40 +0000

    Add HAPI Notified Pull service and configuration for Docker setup

--- example-setup-with-hapi/create-dbs.sql
+++ example-setup-with-hapi/create-dbs.sql
@@ -1,3 +1,5 @@
-create database hapi_directory;
-create database hapi_update_client;
-create database mcsd_update_client;
+CREATE DATABASE hapi_directory;
+CREATE DATABASE hapi_update_client;
+CREATE DATABASE mcsd_update_client;
+CREATE DATABASE hapi_notifiedpull_stu3;
+
